6e3be28a53932a3f3169b48474b4ccfc28a77e47
Michal Pirchala <michal.pirchala@gmail.com>
2020-02-24 15:53:13 +0100

    Implement Images resizing using Imagine lib

--- src/Controller/ImagesController.php
+++ src/Controller/ImagesController.php
@@ -3,6 +3,10 @@ namespace App\Controller;
 
 use App\Controller\AppController;
 use Cake\Http\Exception\BadRequestException;
+use Cake\Http\Exception\InternalErrorException;
+use Cake\Http\Exception\NotFoundException;
+use Imagine\Gd\Imagine;
+use Imagine\Image\Box;
 
 /**
  * Images Controller
@@ -24,8 +28,8 @@ class ImagesController extends AppController
             throw new BadRequestException(__("Invalid width or height"));
         }
 
-        $width = $matches[1];
-        $height = $matches[2];
+        $width = (int)$matches[1];
+        $height = (int)$matches[2];
 
         $gallery = $this->Images->Galleries->findByName($this->request->getParam('gallery'))->first();
         if (empty($gallery)) {
@@ -42,8 +46,28 @@ class ImagesController extends AppController
             throw new NotFoundException(__("Image not found"));
         }
 
-        $this->set('image', $image);
+        $imagePath = WWW_ROOT . "files/Images/filename" . DS . $image->get('filename');
 
-        return null;
+        try {
+            $imagine = new Imagine();
+            $image = $imagine->open($imagePath);
+
+            //resize
+            if ($width != 0 && $height != 0) {
+                $image->resize(new Box($width, $height));
+            } elseif ($width != 0) {
+                $image->resize($image->getSize()->widen($width));
+            } elseif ($height != 0) {
+                $image->resize($image->getSize()->heighten($height));
+            }
+
+            $options = [
+                'jpeg_quality' => 90,
+            ];
+
+            return $this->response->withType('image/jpeg')->withStringBody($image->get('jpg', $options));
+        } catch (\Exception $e) {
+            throw new InternalErrorException(__("Imagine error"));
+        }
     }
 }
