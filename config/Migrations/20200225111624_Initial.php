5e91a5a40149a65cce20e13f063bae11e407d8b3
Michal Pirchala <michal.pirchala@gmail.com>
2020-02-25 12:19:08 +0100

    Add initial snapshot with migrations

---
+++ config/Migrations/20200225111624_Initial.php
@@ -0,0 +1,51 @@
+<?php
+use Migrations\AbstractMigration;
+
+class Initial extends AbstractMigration
+{
+    public function up()
+    {
+
+        $this->table('galleries')
+            ->addColumn('name', 'string', [
+                'default' => null,
+                'limit' => 100,
+                'null' => false,
+            ])
+            ->create();
+
+        $this->table('images')
+            ->addColumn('gallery_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => false,
+            ])
+            ->addColumn('name', 'string', [
+                'default' => null,
+                'limit' => 100,
+                'null' => false,
+            ])
+            ->addColumn('filename', 'string', [
+                'default' => null,
+                'limit' => 100,
+                'null' => false,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->create();
+    }
+
+    public function down()
+    {
+        $this->table('galleries')->drop()->save();
+        $this->table('images')->drop()->save();
+    }
+}
